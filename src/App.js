562b83af50d435cdf6f6f12f6d6fbee8680e0c19
Esteban <esteban.github@gmail.com>
2021-09-21 18:15:15 +0200

    add iteration 3

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import { useState } from 'react';
 
 
 const initialArr = contacts.slice(0,5);
-//console.log(initialArr);
+
 
 function App() {
 
-  const [contacts, setContacts] = useState(initialArr);
+  const [contactsState, setContactsState] = useState(initialArr);
 
-  const contactsList = contacts.map(contact => (
+  const contactsList = contactsState.map(contact => (
     <tr key={contact.id}>
-      <td><img src={contact.pictureUrl} height='120px'></img></td>
+      <td><img src={contact.pictureUrl} height='120px' alt='Artist face'></img></td>
       <td>{contact.name}</td>
       <td>{contact.popularity.toFixed(2)}</td>
       <td>{contact.wonOscar && ' 🏆 '}</td>
@@ -21,10 +21,32 @@ function App() {
     </tr>
   ))
 
+
+  const addRandomContact = () => {
+    let ranCon =  contacts[Math.floor(Math.random()*contacts.length)];
+    if (contactsState.length === contacts.length) {
+      alert('No more contacts available');
+      return;
+    } else if (contactsState.includes(ranCon)) {
+      console.log(ranCon.name + 'is included already. Trying with other...');
+      addRandomContact();
+    } else {
+      setContactsState(contactsState => [ranCon, ...contactsState])
+    }
+  }
+
+
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      
+      <h5>Total contacts available: {contacts.length}</h5>
+      <h5>Showing: {contactsState.length}</h5>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <br />
+      <br />
+      <br />
+
       <table>
         <thead>
           <tr>
